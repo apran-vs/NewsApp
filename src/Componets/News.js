c25e30e2e822e4b201602b85e1055e87cb2c4825
Arpan Shingala <202003013@daiict.ac.in>
2022-01-28 15:14:22 +0530

    CheckPoint7

--- src/Componets/News.js
+++ src/Componets/News.js
@@ -2,25 +2,51 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-
   constructor() {
     super();
     this.state = {
       articles: [],
       loading: false,
+      page: 1,
     };
   }
 
-  async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=ec9b8633b1634e469b4652cb03570cd2";
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=ec9b8633b1634e469b4652cb03570cd2&page=1&pageSize=20";
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData)
+    console.log(parsedData);
     this.setState({
-      articles: parsedData.articles
-    })
-    console.log(this.articles)
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+    console.log(this.articles);
   }
+
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ec9b8633b1634e469b4652cb03570cd2&page=${this.state.page - 1}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 <= Math.ceil(this.state.totalResults / 20)) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=ec9b8633b1634e469b4652cb03570cd2&page=${this.state.page + 1}&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
 
   render() {
     return (
@@ -31,14 +57,36 @@ export class News extends Component {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title?element.title:""}
-                  description={element.description?element.description:""}
-                  imageUrl={element.urlToImage?element.urlToImage:"https://images.indianexpress.com/2022/01/horoscope_2-1200-1-1.jpeg"}
-                  newsUrl={element.url?element.url:""}
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://images.indianexpress.com/2022/01/horoscope_2-1200-1-1.jpeg"
+                  }
+                  newsUrl={element.url ? element.url : ""}
                 />
               </div>
             );
           })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            disabled={this.state.page >= Math.ceil(this.state.totalResults/20)}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
